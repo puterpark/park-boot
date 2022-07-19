468ed05ab4fc27f3575902b1561ebbe4524b878c
Puter <puterpark@me.com>
2022-07-19 21:39:49 +0900

    fix: 배포 방식 변경 - jar → war

--- src/main/java/us/puter/park/ParkApplication.java
+++ src/main/java/us/puter/park/ParkApplication.java
@@ -2,12 +2,18 @@ package us.puter.park;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ParkApplication {
+public class ParkApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ParkApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkApplication.class, args);
 	}
-
 }
