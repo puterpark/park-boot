4f356e8a8f78cdd0d69305ca3281e225d23aab6b
Puter <puterpark@me.com>
2022-09-04 22:36:28 +0900

    refactor: redis 캐시 적용
    - Menu 엔티티의 직렬화 상속 제거

--- src/main/java/us/puter/park/domain/entity/Menu.java
+++ src/main/java/us/puter/park/domain/entity/Menu.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity(name = "menu")
 @Getter
 @Setter
-public class Menu implements Serializable {
+public class Menu {
 
 	@Id
 	@Column(name = "menuUid")
