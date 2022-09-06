18dfb67e8d54b80598d1786522187260f2f14796
Puter <puterpark@me.com>
2022-09-06 15:11:42 +0900

    refactor: redis 캐시
    - RedisConfig에서 @EnableCaching 삭제
     > 실제 사용되고 있지 않기 때문

--- src/main/java/us/puter/park/config/RedisConfig.java
+++ src/main/java/us/puter/park/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package us.puter.park.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,7 +12,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import us.puter.park.util.Utility;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
 	@Value("${spring.redis.host}")
