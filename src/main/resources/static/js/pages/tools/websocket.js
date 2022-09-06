cc41c5f335f6ce78b082599ddc0584c5561295e9
Puter <puterpark@me.com>
2022-09-06 18:36:52 +0900

    feat: websocket 채팅 구현 #3
    - ws → wss

--- src/main/resources/static/js/pages/tools/websocket.js
+++ src/main/resources/static/js/pages/tools/websocket.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://' + window.location.hostname + ':' + window.location.port + '/test/websocket');
+const socket = new WebSocket('wss://' + window.location.hostname + ':' + window.location.port + '/test/websocket');
 
 socket.onopen = function (e) {
 	console.log('open server.');
